5fbcd96a23464029499cab0ee5686ab449b2ca2b
pawelszpunar <78647588+pawelszpunar@users.noreply.github.com>
2024-03-03 20:44:14 +0100

    route protection, automatic logout

--- src/pages/Root.js
+++ src/pages/Root.js
@@ -1,9 +1,20 @@
-import { Outlet, useNavigation } from 'react-router-dom';
+import { Outlet, useLoaderData, useSubmit } from 'react-router-dom';
 
 import MainNavigation from '../components/MainNavigation';
+import { useEffect } from 'react';
 
 function RootLayout() {
+  const token = useLoaderData();
+  const submit = useSubmit();
   // const navigation = useNavigation();
+  useEffect(() => {
+    if(token) {
+      return;
+    }
+    setTimeout(() => {
+      submit(null, {action: '/logout', method: 'post'})
+    }, 1 * 60 * 60 * 1000); //  1 * 60 * 60 * 1000 => 1 hour
+  }, [token, submit]);
 
   return (
     <>
